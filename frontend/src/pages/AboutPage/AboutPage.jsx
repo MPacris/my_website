d2b06f12877df5435c09e856d2252b2789b22991
MPacris <mpacris@yahoo.com>
2023-08-22 12:04:33 -0500

    import of bootstrap and adjust template of about me

--- frontend/src/pages/AboutPage/AboutPage.jsx
+++ frontend/src/pages/AboutPage/AboutPage.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import './AboutPage.css'; // You can define your styles in this CSS file
 
+
 const AboutPage = () => {
   return (
-    <div className="about-page">
-      <div className="top-left">
-        {/* Place your image here */}
-        <img src="/assets/MeynardPacris.jpg" alt="Top Left" className="profile" />
-      </div>
-      <div className="top-right">
-        {/* Text in top middle and top right */}
-        <div className="top-middle">
-          <h1>About Us</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8">
+          {/* Place your image here */}
+          <img src="/assets/MeynardPacris.jpg" alt="profile" className="profile" />
         </div>
-        <div className="top-right-text">
-          <p>This is the about us section where you can describe your company or team.</p>
+        <div className="col-md-4">
+          {/* Text in top middle and top right */}
+          <div className="row">
+            <div className="col-md-12">
+              <h1>About Me</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <p>This is the about us section where you can describe your company or team.</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="bottom-row">
-        {/* Multiple images in the bottom row */}
-        <div className="image-flex-container">
-          <img src="image_url" alt="Image 1" />
-          <img src="image_url" alt="Image 2" />
-          <img src="image_url" alt="Image 3" />
+      <div className="row">
+        <div className="col-md-12">
+          {/* Multiple images in the bottom row */}
+          <div className="d-flex justify-content-between">
+            <img src="image_url" alt="Image 1" className="img-fluid" />
+            <img src="image_url" alt="Image 2" className="img-fluid" />
+            <img src="image_url" alt="Image 3" className="img-fluid" />
+          </div>
         </div>
       </div>
     </div>
