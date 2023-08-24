640dfce2883ffe2cd3a3d09538bb5ecb26ed92b1
MPacris <mpacris@yahoo.com>
2023-08-24 15:40:30 -0500

    updated splidepage

--- frontend/src/pages/SplidePage/SplidePage.jsx
+++ frontend/src/pages/SplidePage/SplidePage.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-import "./SplidePage.css"
 import '@splidejs/react-splide/css';
 
-export default () => {
+const SplidePage= () => {
+
+  
+
   return (
 
     <Splide
       options={ {
         rewind: true,
         gap   : '1rem',
-        height: 600,
+        perPage: 1,
+        maxheight: 600,
         width: 300,
         
       } }
       aria-label="My Favorite Images"
     >
       <SplideSlide>
-        <img src="/assets/IRELAND.JPG" alt="Image 1"/>
+        <img src="/assets/IRELAND.JPG" alt="Image 1" style={{ maxWidth: '100%', height: '100%' }}/>
+        
       </SplideSlide>
       <SplideSlide>
-        <img src="/assets/LSU_alumn.jpg" alt="Image 2"/>
+        <img src="/assets/LSU_alumn.jpg" alt="Image 2" style={{ maxWidth: '100%', height: '100%' }}/>
       </SplideSlide>
       <SplideSlide>
-        <img src="/assets/rollercoasters.jpg" alt="Image 3"/>
+        <img src="/assets/rollercoasters.jpg" alt="Image 3" style={{ maxWidth: '100%', height: '100%' }}/>
       </SplideSlide>
       <SplideSlide>
-        <img src="/assets/love_the_beach.jpg" alt="Image 4"/>
+        <img src="/assets/love_the_beach.jpg" alt="Image 4" style={{ maxWidth: '100%', height: '100%' }}/>
       </SplideSlide>
     </Splide>
 
   );
 }
+
+export default SplidePage;
