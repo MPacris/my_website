60c3eeceb000f88ca12022fab0ec44dcd5949011
MPacris <mpacris@yahoo.com>
2023-09-03 09:51:49 -0500

    updates to the career page adding in descriptions and resetting the layout

--- frontend/src/pages/CareerPage/CareerPage.jsx
+++ frontend/src/pages/CareerPage/CareerPage.jsx
@@ -21,13 +21,13 @@ const CareerPage = () => {
             <div className="back">
               <div className="details">
                 <div className="caption">
-                  " Saint Patrick's Cathedral in Dublin, Ireland, founded in 1191 as a Roman Catholic cathedral, is currently the national cathedral of the Church of Ireland. Christ Church Cathedral, also a Church of Ireland cathedral in Dublin, is designated as the local cathedral of the Diocese of Dublin and Glendalough "
+                Originally a Theatre major (long story), I chose to change my major to Political Science and minor in Speech Communication as a path to pre-law.  My coursework consisted of research projects and presentations.  One of my favorite projects was based on the media’s influence on polling numbers and how demographics shaped campaigns.  This is where I believe my passionate curiosity, analytical mindset, and ability to be comfortable while being uncomfortable began to take shape and become strengths I could rely on in the future.
                 </div>
                 <span>1998-2002</span>
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -37,13 +37,13 @@ const CareerPage = () => {
             <div className="back">
               <div className="details">
                 <div className="caption">
-                  " Saint Patrick's Cathedral in Dublin, Ireland, founded in 1191 as a Roman Catholic cathedral, is currently the national cathedral of the Church of Ireland. Christ Church Cathedral, also a Church of Ireland cathedral in Dublin, is designated as the local cathedral of the Diocese of Dublin and Glendalough "
+                What started off as a part-time job while pursuing my undergrad, later turned into becoming a full-time position with aspirations to become manager after graduation.  I was drawn in to the idea of being someone who started from the bottom and became a manager and running my own store.  Eventually, I would be the youngest manager in our district at the time and my store would go on to win multiple sales awards. This experience as a manager strenghtened my abilities as a leader, collaborator, and motivator.  I would leave Champs in 2005 with a new mindset and focus on business and organizational behavior management as I pursued a MBA at the University of New Orleans.
                 </div>
                 <span>2001-2005</span>
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -53,13 +53,13 @@ const CareerPage = () => {
             <div className="back">
               <div className="details">
                 <div className="caption">
-                  " Saint Patrick's Cathedral in Dublin, Ireland, founded in 1191 as a Roman Catholic cathedral, is currently the national cathedral of the Church of Ireland. Christ Church Cathedral, also a Church of Ireland cathedral in Dublin, is designated as the local cathedral of the Diocese of Dublin and Glendalough "
+                The pursuit of my MBA at the University of New Orleans started off with a true test of character and will.  In August of 2005, I was only one week into classes before having to evacuate to Baton Rouge for Hurricane Katrina.  Little did I know that whatever I packed into my car was all I would have left.  It took deep faith and a strong support group to stay motivated and driven to keep pursuing my MBA.  I was able to stay the course and was able to enroll for one semester at LSU.  My second semester was back home at UNO with on-line courses.  Many of the professors had to pivot to on-line coursework which was new to them as well as the students.  In December 2007, I graduated with strengthened skills in resourcefulness, autonomous learning, technological savviness, a newfound liking for data analytics, and most importantly resilience. 
                 </div>
                 <span>2005-2007</span>
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -69,13 +69,13 @@ const CareerPage = () => {
             <div className="back">
               <div className="details">
                 <div className="caption">
-                  " Saint Patrick's Cathedral in Dublin, Ireland, founded in 1191 as a Roman Catholic cathedral, is currently the national cathedral of the Church of Ireland. Christ Church Cathedral, also a Church of Ireland cathedral in Dublin, is designated as the local cathedral of the Diocese of Dublin and Glendalough "
+                While pursuing my MBA, I found myself working for a contractor gutting houses in the Lakeview area of New Orleans.  There was something about being able to take part in the rebuilding process in New Orleans that made me feel a deeper sense of connection and appreciation for the city and culture.  I would eventually go from gutting houses to working with the carpenters learning how to do trim work, then working with the sheet rockers, plumbers, and painters.  My favorite thing to do was sanding, staining, and finishing woodwork like doors, stairs, rails, and cabinets.  Seeing something go from gutted, to reframed, to rebuilt and then the finishing touches was the most rewarding part.  I left the job in the spring of 2008 in pursuit of a professional career.  (New Orleans summers in the heat are brutal). I came out of the experience with a better understanding and appreciation of project management.  This was also where I started to believe in my ability to learn anything. 
                 </div>
                 <span>2006-2008</span>
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -85,13 +85,13 @@ const CareerPage = () => {
             <div className="back">
               <div className="details">
                 <div className="caption">
-                  " Saint Patrick's Cathedral in Dublin, Ireland, founded in 1191 as a Roman Catholic cathedral, is currently the national cathedral of the Church of Ireland. Christ Church Cathedral, also a Church of Ireland cathedral in Dublin, is designated as the local cathedral of the Diocese of Dublin and Glendalough "
+                In 2008, during the recession, it was tough trying to find meaningful employment in the New Orleans area.  I found myself drawing a line in the sand that if I was unable to find a job in New Orleans by May 1, I would leave the city and head to Houston where many of my friends had settled post Katrina.  On April 28, I was offered I position with Reily Foods Company to start on May 1.  I was hired into the position as Supply Chain Supervisor which would be the back up to the Production Planning Manager and Transportation Manager.  In my first month, the two managers that I was backing up both went on maternity leave.  From there, it was trial by fire.  Learn on the go.  A few months after everything settled out, my day-to-day activities included scheduling transfers of finished goods between warehouses, arranging inbound freight, creating and maintaining carrier scorecards, performing analysis on warehousing and freight costs, and occasionally scheduling production as needed. As supply chain supervisor, I had taken on a role that never existed before and was lucky to have a VP of Operations that allowed me to grow the position and take on more responsibility as my skillset and abilities were proven.  Aside from my day to day, I was able to work on cross functional projects where I was able to sharpen my research and analysis skill set.  At this point, I was in a visible role amongst our plants, distribution centers, freight carriers, accounting department, and IT department.  This was where my professionalism, accountability, collaborative mindset, ethical beliefs, and resourcefulness began to grow as my core competencies and values.
                 </div>
                 <span>2008-2011</span>
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -101,13 +101,13 @@ const CareerPage = () => {
             <div className="back">
               <div className="details">
                 <div className="caption">
-                  " Saint Patrick's Cathedral in Dublin, Ireland, founded in 1191 as a Roman Catholic cathedral, is currently the national cathedral of the Church of Ireland. Christ Church Cathedral, also a Church of Ireland cathedral in Dublin, is designated as the local cathedral of the Diocese of Dublin and Glendalough "
+                As my visibility in the company grew and my skillset was in higher demand, I was promoted to a new position created for me as the Vendor Managed Inventory Specialist.  At the time, customers such as Walmart, Food Lion and Kroger placed the responsibility of ensuring in store and warehouse levels of inventory were maintained by the supplier.  To do this, suppliers had to navigate customer portals, research inventory levels, provide analysis on historical sales to adjust forecasts, and provide replenishment orders.  Eventually, Walmart was the only customer that we maintained this relationship with.  In this role, I worked closely with Walmart buyers and replenishment managers to ensure that rollbacks, special events, and new item introductions were stocked appropriately.  As Walmart transitioned vendors to being collect freight terms vendors where their own fleet would arrange pick up from suppliers, I was responsible for providing the analysis for the transition and the point person for handling issues or concerns between our distribution centers and the Walmart fleet teem.   To maintain a high Supply Chain Reliability score, I was responsible for researching and maintaining correct lead times and managing any freight claims or deductions.  In this position, I was also able to interview and provide insight on hiring my replacement for my previous position and this position when it was time to move on.  I was also responsible for training those replacements.  I always believed that you should always leave a position better than how you received it.  In my case, these positions were created for me so I was able to provide insight into who my replacement would be.  At this point in my career, I had now gained greater visibility in the company with Sales and Marketing and was seen as someone who was comfortable being customer forward facing.  My skills at providing analysis and presenting to others outside of our department and company were developed.
                 </div>
                 <span>2011-2013</span>
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -123,7 +123,7 @@ const CareerPage = () => {
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -139,7 +139,7 @@ const CareerPage = () => {
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
         <div className="col-sm-4">
           <div className="card">
@@ -155,7 +155,7 @@ const CareerPage = () => {
               </div>
             </div>
           </div>
-          {/* Add more cards as needed */}
+
         </div>
       </div>
     </div>
