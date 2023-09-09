a31b6d13f4e05397def2819c0011203edb7e1e93
MPacris <mpacris@yahoo.com>
2023-09-09 16:08:13 -0500

    updated career descriptions moving date and titles above caption

--- frontend/src/pages/CareerPage/CareerPage.jsx
+++ frontend/src/pages/CareerPage/CareerPage.jsx
@@ -19,10 +19,11 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>1998-2002: Bachelor of Arts</span>
                 <div className="caption">
                 Originally a Theatre major (long story), I chose to change my major to Political Science and minor in Speech Communication as a path to pre-law.  My coursework consisted of research projects and presentations.  One of my favorite projects was based on the media’s influence on polling numbers and how demographics shaped campaigns.  This is where I believe my passionate curiosity, analytical mindset, and ability to be comfortable while being uncomfortable began to take shape and become strengths I could rely on in the future.
                 </div>
-                <span>1998-2002</span>
+
               </div>
             </div>
           </div>
@@ -35,10 +36,11 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>2001-2005: Retail Store Management</span>
                 <div className="caption">
                 What started off as a part-time job while pursuing my undergrad, later turned into becoming a full-time position with aspirations to become manager after graduation.  I was drawn in to the idea of being someone who started from the bottom and became a manager and running my own store.  Eventually, I would be the youngest manager in our district at the time and my store would go on to win multiple sales awards. This experience as a manager strenghtened my abilities as a leader, collaborator, and motivator.  I would leave Champs in 2005 with a new mindset and focus on business and organizational behavior management as I pursued a MBA at the University of New Orleans.
                 </div>
-                <span>2001-2005</span>
+
               </div>
             </div>
           </div>
@@ -51,10 +53,11 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>2005-2007: Masters of Business Administration</span>
                 <div className="caption">
                 The pursuit of my MBA at the University of New Orleans started off with a true test of character and will.  In August of 2005, I was only one week into classes before having to evacuate to Baton Rouge for Hurricane Katrina.  Little did I know, that whatever I packed into my car was all I would have left.  It took deep faith and a strong support group to stay motivated and driven to keep pursuing my MBA.  I was able to stay the course and was able to enroll for one semester at LSU.  My second semester was back home at UNO with on-line courses.  Many of the professors had to pivot to on-line coursework which was new to them as well as the students.  In December 2007, I graduated with strengthened skills in resourcefulness, autonomous learning, technological savviness, a newfound liking for data analytics, and most importantly resilience. 
                 </div>
-                <span>2005-2007</span>
+
               </div>
             </div>
           </div>
@@ -67,10 +70,11 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>2006-2008: General Laborer</span>
                 <div className="caption">
                 While pursuing my MBA, I found myself working for a contractor gutting houses in the Lakeview area of New Orleans.  There was something about being able to take part in the rebuilding process in New Orleans that made me feel a deeper sense of connection and appreciation for the city and culture.  I would eventually go from gutting houses to working with the carpenters learning how to do trim work, then working with the sheet rockers, plumbers, and painters.  My favorite thing to do was sanding, staining, and finishing woodwork like doors, stairs, rails, and cabinets.  Seeing something go from gutted, to reframed, to rebuilt and then the finishing touches was the most rewarding part.  I left the job in the spring of 2008 in pursuit of a professional career.  (New Orleans summers in the heat are brutal). I came out of the experience with a better understanding and appreciation of project management.  This was also where I started to believe in my ability to learn anything. 
                 </div>
-                <span>2006-2008</span>
+          
               </div>
             </div>
           </div>
@@ -83,10 +87,11 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>2008-2011: Supply Chain Supervisor</span>
                 <div className="caption">
                 In 2008, during the recession, it was tough trying to find meaningful employment in the New Orleans area.  I found myself drawing a line in the sand that if I was unable to find a job in New Orleans by May 1, I would leave the city and head to Houston where many of my friends had settled post Katrina.  On April 28, I was offered I position with Reily Foods Company to start on May 1.  I was hired into the position as Supply Chain Supervisor which would be the back up to the Production Planning Manager and Transportation Manager.  In my first month, the two managers that I was backing up both went on maternity leave.  From there, it was trial by fire.  Learn on the go.  A few months after everything settled out, my day-to-day activities included scheduling transfers of finished goods between warehouses, arranging inbound freight, creating and maintaining carrier scorecards, performing analysis on warehousing and freight costs, and occasionally scheduling production as needed. As supply chain supervisor, I had taken on a role that never existed before and was lucky to have a VP of Operations that allowed me to grow the position and take on more responsibility as my skillset and abilities were proven.  Aside from my day to day, I was able to work on cross functional projects where I was able to sharpen my research and analysis skill set.  At this point, I was in a visible role amongst our plants, distribution centers, freight carriers, accounting department, and IT department.  This was where my professionalism, accountability, collaborative mindset, ethical beliefs, and resourcefulness began to grow as my core competencies and values.
                 </div>
-                <span>2008-2011</span>
+   
               </div>
             </div>
           </div>
@@ -99,10 +104,11 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>2011-2013: Vendor Managed Inventory Specialist</span>
                 <div className="caption">
                 As my visibility in the company grew and my skillset was in higher demand, I was promoted to a new position created for me as the Vendor Managed Inventory Specialist.  In this role, had to navigate customer portals, research inventory levels, provide analysis on historical sales to adjust forecasts to create replenishment orders. The focus of this role was working closely with Walmart buyers and replenishment managers to ensure that rollbacks, special events, and new item introductions were stocked appropriately.  As Walmart transitioned vendors to being collect freight terms vendors where their own fleet would arrange pick up from suppliers, I was responsible for providing the analysis for the transition and the point person for handling issues or concerns between our distribution centers and the Walmart fleet teem.   To maintain a high Supply Chain Reliability score, I was responsible for researching and maintaining correct lead times and managing any freight claims or deductions.  In this position, I was also able to interview and provide insight on hiring my replacement for my previous position and this position when it was time to move on.  I was also responsible for training those replacements.  I always believed that you should always leave a position better than how you received it.  In my case, these positions were created for me so I was able to provide insight into who my replacement would be.  At this point in my career, I had now gained greater visibility in the company with Sales and Marketing and was seen as someone who was comfortable being customer forward facing.  My skills at providing analysis and presenting to others outside of our department and company were developed.
                 </div>
-                <span>2011-2013</span>
+
               </div>
             </div>
           </div>
@@ -115,10 +121,11 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>2013-2016: Operations Business Manager</span>
                 <div className="caption">
                As the Purchasing Manager retired, a new position was created to combine purchasing and production planning functionality, and I assumed the role of Operations Business Manager (OBM). In this capacity, I oversaw the entire supply chain for mayonnaise, sauces, and coffee brands. This encompassed purchasing, inventory management, contract negotiations for raw materials, production scheduling, inventory maintenance, and the project management for the introduction of new finished goods.  During my tenure as OBM, I negotiated multiple contracts, conducted RFP bids to lower prices and reduce minimum order quantities for raw materials, and collaborated with manufacturing facilities to optimize production cycles and enhance efficiency through equipment upgrades. Additionally, I led coffee private label bids, working with cross-functional teams on feasibility, pricing, project expectations, and timelines.  At times, I accompanied Sales and Finance to represent Operations and Quality in top-to-top presentations at customer headquarters during bid processes.  Upon successful bids, I managed sample approval production runs, reviewed and approved customer graphics, coordinated with packaging suppliers, scheduled production trials, and ensured first production met customer deadlines and expectations. For branded new products, I collaborated with various teams to determine feasibility, costing, and timelines. During development, I adjusted production schedules, sourced trial quantities of new ingredients, and coordinated with packaging suppliers to accommodate production trials. After approval, I led the review and approval of graphics, ordered initial quantities of printed materials, and adjusted schedules to meet project deadlines. Subsequently, I scheduled process improvement trials and maintained constant communication with Sales to align forecasts with production schedules for new products.  As OBM, I also collaborated with IT to invest in reporting and forecasting tools to be able to provide data visualization to Senior Leadership in S&OP meetings.  At this point, I had a good grasp of supply chain management, my analysis and presentation skills had grown exponentially, and I also added negotiation and project management to my repertoire.
                 </div>
-                <span>2013-2016</span>
+
               </div>
             </div>
           </div>
@@ -131,10 +138,12 @@ const CareerPage = () => {
             </div>
             <div className="back">
               <div className="details">
+              <span>2016-2022:  Corporate Audit/ Document Manager</span>  
                 <div className="caption">
+
 At the end of 2016, as the Quality department moved out from underneath the Operations department, I was presented with the opportunity to take on a newly created role of Corporate Audit/ Document Manager.  In this role, I applied a hybrid project management approach that blended traditional/waterfall, agile/scrum, and six sigma methodologies to lead continuous improvement projects, ensure internal Reily sites were prepared for audits and maintained quality standards, and collaborate with new copackers to meet product launch requirements. Additionally, I managed cross-functional teams during Rainforest Alliance Certification projects, ensuring collaboration across departments.  In this role I conducted food safety and quality audits on co-packers, suppliers, and our own Reily Foods Company sites, ensuring compliance with GFSI and FDA standards.  To enhance our document management capabilities, I was responsible for the implementation of a document management program utilizing TraceGains which significantly improved the supplier approval and monitoring process.  Another responsibility I took on was managing supplier and copacker nonconformances.  I engaged in collaborative efforts with suppliers, implementing root cause analysis and corrective action strategies to resolve issues effectively.  I also utilized Power BI reports to review nonconformance and defect data, working closely with site QA Managers to drive continuous improvement efforts. My collaboration with site QA Managers also extended to creating HACCP/Food Safety Plans, devising international adulteration risk assessments, formulating supply chain vulnerability assessments, and leading continuous improvement initiatives.  I also created an auditing tool within Redzone which leveraged Tableau to deliver actionable insights and data visualization. My proficiency in utilizing SQL reports was instrumental in leading initiatives for conducting Traceability Exercises within Microsoft Dynamics AX. Furthermore, I took charge of mock recalls and incident reporting activities, utilizing SQL reports, and fostering cross-functional collaboration to ensure effective outcomes. Although my title had ‘audit/ document manager’ in it, I wore many hats and led projects well beyond my initial job description.  
 </div>
-                <span>2016-2022</span>
+
               </div>
             </div>
           </div>
@@ -147,10 +156,12 @@ At the end of 2016, as the Quality department moved out from underneath the Oper
             </div>
             <div className="back">
               <div className="details">
+
+              <span>2023:  devCodeCamp Full Stack Developer Bootcamp</span>
                 <div className="caption">
                  After being laid off in November of 2022, I decided to take a few months off to rethink my next career move.  In my previous roles, I took those on to fill a need at the company.  After giving it a lot of thought, I wanted to find a way to pivot into a career in the technology industry.  As someone who witnessed the dawn of a new era coming from the days of commodore 64s and DOS commands, Oregon Trail, Duke Nukem 3D LAN in the computer lab, dial up modems, Prodigy, AOL chatrooms, T1 connections in the dorms, Napster, Limewire crashing the family computer, coding on Myspace and making sure my top 8 was a representation of who I am, etc… I wanted to take a shot in getting into tech industry.  That led me to pursuing a fullstack developer bootcamp.  At devCodeCamp, I was exposed to programming languages and technologies, including Python, Django, Java, Spring, JavaScript, HTML, CSS, React.js, Flask, and many others. I also gained hands-on experience with essential tools and practices like AWS, Docker, Git & GitHub, Jenkins, Kubernetes, and more.  My education at devCodeCamp was a 16-week, project-based coding bootcamp that focused on computer science concepts, building real-world full-stack applications, and deploying them to the cloud utilizing DevOps best practices. Throughout the program, I honed my skills in various programming languages, object-oriented programming, algorithm development, web design and development, testing, agile methodologies, and more.  My experience at devCodeCamp provided me with a strong foundation in full-stack development, equipped me with the skills and knowledge needed to hopefully break into the technology industry, and allowed me to work on meaningful projects that showcased my abilities.  Most importantly, it was something that I had to do to prove to myself that I still had the analytical skills, learning capability, and competitive edge to take on whatever my new career path throws my way.  It is my resilience and abilities that I have faith in that make me believe that I can be a valuable asset to any organization that takes a chance on me. 
                 </div>
-                <span>2023</span>
+   
               </div>
             </div>
           </div>
